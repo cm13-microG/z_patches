ff24bd2aa91ef93e28040b057250513035c2a79c
mse1969 <mse1969@posteo.de>
2017-12-28 20:26:12 +0100

    adapt build scripts f. microG
    Change-Id: I849748f0eda89ae9f4f3290737b2c78bf7a8659a

--- build_gts210ltexx.sh
+++ build_gts210ltexx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Sample script to build the LOS 13 Osprey ROM
-# Copy into the root dir of your build tree
-# and feel free to adapt to your own needs
-# ------------------------------------------
+# Sample script to build the LOS 13 Osprey ROM (microG build)
+# Copy into the root dir of your build tree and feel free to 
+# adapt to your own needs
+# ----------------------------------------------------------
 
 # Initiate environment
 source build/envsetup.sh
@@ -16,9 +16,11 @@ export USE_CCACHE=1
 #export CCACHE_DIR=~/android/.ccache
 prebuilts/misc/linux-x86/ccache/ccache -M 32G
 
-# comment below line, if you want to build 
-# without root baked in
-export WITH_SU=true
+# un-comment below line, if you want to build with root baked in
+# export WITH_SU=true
+
+# microG build
+export RELEASE_TYPE=microG
 
 # Normalize build metadata
 export KBUILD_BUILD_USER=android
--- build_osprey.sh
+++ build_osprey.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Sample script to build the LOS 13 Osprey ROM
-# Copy into the root dir of your build tree
-# and feel free to adapt to your own needs
-# ------------------------------------------
+# Sample script to build the LOS 13 Osprey ROM, microG build
+# Copy into the root dir of your build tree and feel free to 
+# adapt according to your own needs
+# ----------------------------------------------------------
 
 # Initiate environment
 source build/envsetup.sh
@@ -17,13 +17,16 @@ export USE_CCACHE=1
 #export CCACHE_DIR=~/android/.ccache
 prebuilts/misc/linux-x86/ccache/ccache -M 32G
 
-# Adjust TOOLCHAIN path for MSM8916
+# Adjust TOOLCHAIN path for MSM8916 & Osprey custom Kernel
+#export KERNEL_TOOLCHAIN=$PWD/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
 export KERNEL_TOOLCHAIN=$PWD/prebuilts/gcc/linux-x86/arm/linaro-arm-eabi-6.4/bin
 export KERNEL_TOOLCHAIN_PREFIX=arm-eabi-
 
-# comment below line, if you want to build 
-# without root baked in
-export WITH_SU=true
+# remove comment from below line, if you want to build with root baked in
+# export WITH_SU=true
+
+# microG build
+export RELEASE_TYPE=microG
 
 # Normalize build metadata
 export KBUILD_BUILD_USER=android

d042bcb9c4efc41eb5b38f3e42b695889501fdbd
mse1969 <mse1969@posteo.de>
2018-02-04 16:57:21 +0100

    Switch to cm13-microG organization
    Repositories in branch 'cm-13.0-microG' contain already
    the patches, so patchfiles removed
    
    Change-Id: Ie861b0c184e5881e425cecd22ea5717ec0206393

--- patches_apply.sh
+++ patches_apply.sh
@@ -13,52 +13,6 @@ patch -p1 < $THISDIR/patch_001_metadata-build.patch
 echo "--------------------------------------------------------"
 cd $TOPDIR
 
-cd external/sqlite
-echo "--------------------------------------------------------"
-echo "Patching $PWD (SECURE DELETE)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_002_securedelete-sqlite.patch
-echo "--------------------------------------------------------"
-cd $TOPDIR
-
-cd frameworks/base
-echo "--------------------------------------------------------"
-echo "Patching $PWD (SECURE DELETE)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_002_securedelete-base.patch
-echo "--------------------------------------------------------"
-echo "Patching $PWD (microG restricted)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_003_microG-restr-base.patch
-echo "--------------------------------------------------------"
-echo "Patching $PWD (SET_TIME_ZONE)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_004_timezone-base.patch
-echo "--------------------------------------------------------"
-echo "Patching $PWD (Motion Sensors)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_005_sensor-base.patch
-echo "--------------------------------------------------------"
-cd core/res/res/values
-rm *orig
-cd $TOPDIR
-
-cd frameworks/native
-echo "--------------------------------------------------------"
-echo "Patching $PWD (Motion Sensors)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_005_sensor-native.patch
-echo "--------------------------------------------------------"
-cd $TOPDIR
-
-cd packages/apps/Settings
-echo "--------------------------------------------------------"
-echo "Patching $PWD (Motion Sensors)"
-echo ".  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . "
-patch -p1 < $THISDIR/patch_005_sensor-Settings.patch
-echo "--------------------------------------------------------"
-cd $TOPDIR
-
 
 cd $THISDIR
 
